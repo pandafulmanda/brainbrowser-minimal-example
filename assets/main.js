c8b280136048280f2cabf045fd76cf3f8d704ef8
pandafulmanda <pandafulmanda@gmail.com>
2016-07-17 19:54:58 -0500

    even more minimal

--- assets/main.js
+++ assets/main.js
@@ -1,20 +1,17 @@
-(function startSurfaceViewer() {
-  "use strict";
+BrainBrowser.config.set("worker_dir", "./brainbrowser-2.5.0/workers/");
+BrainBrowser.SurfaceViewer.start('brainbrowser', handleBrainz);
 
-  BrainBrowser.SurfaceViewer.start('brainbrowser', handleBrainz);
+// Pulled out this function from the start call so that it's not so nested.
+function handleBrainz(viewer) {
+  //Add an event listener.
+  viewer.addEventListener('displaymodel', function(brainBrowserModel) {
+    console.log('We have a model!');
+  });
 
-  function handleBrainz(viewer) {
-    //Add an event listener.
-    viewer.addEventListener('displaymodel', function(brainBrowserModel) {
-      console.log('We have a model!');
-    });
+  // Start rendering the scene.
+  viewer.render();
 
-    // Start rendering the scene.
-    viewer.render();
+  // Load a model into the scene.
+  viewer.loadModelFromURL('./models/brain-surface.obj.gz');
 
-    // Load a model into the scene.
-    viewer.loadModelFromURL('./models/brain-surface.obj.gz');
-
-  }
-
-})();
+}
