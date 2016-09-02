fed85c8b9a053eda905c51cb933fb966af280c0c
Anisha Keshavan <akeshavan@ucla.edu>
2016-09-01 22:31:59 -0700

    enh: colormap swapper

--- assets/main.js
+++ assets/main.js
@@ -32,7 +32,8 @@ var inputs = queryStringToHash();
 
 var modelUrl = inputs.model || './models/vtk/freesurfer_curvature.vtk'
 var overlayUrl = inputs.overlay || './models/vertices.csv'
-
+var colormaps = {}
+BrainBrowser.config.get("color_maps").forEach(function(val, idx, arr){colormaps[val.name] = val.url})
 
 // Pulled out this function from the start call so that it's not so nested.
 function handleBrainz(viewer) {
@@ -53,13 +54,14 @@ function handleBrainz(viewer) {
     var model_data = event.model_data;
     var intensity_data = event.intensity_data;
     intensity_data.transparency = 1
+    intensity_data.colormap_name = "Spectral"
     window.intensityData = intensity_data;
     overlayGui = meshgui.addFolder(intensity_data.name);
     overlayGui.open();
     var vmin = overlayGui.add(intensity_data, 'min');
     var vmax = overlayGui.add(intensity_data, 'max');
     var transparency = overlayGui.add(intensity_data, 'transparency',0,1);
-
+    var cmap = overlayGui.add(intensity_data, "colormap_name", Object.keys(colormaps))
     vmin.onChange(function(newMin){
       viewer.setIntensityRange(newMin, intensity_data.max)
     })
@@ -69,11 +71,14 @@ function handleBrainz(viewer) {
     transparency.onChange(function(newT){
         viewer.setTransparency(newT, {shape_name: model_data.name})
     })
+    cmap.onChange(function(newC){
+        viewer.loadColorMapFromURL(colormaps[newC])
+    })
     
   });
 
   viewer.addEventListener("loadcolormap", function(event) {
-    viewer.color_map.clamp = false;      
+    viewer.color_map.clamp = false; 
   });
 
   // Start rendering the scene.
